435222be7628da37f73b625bfc5b35a9d164275e
Huy Ly <huylg2109@gmail.com>
2023-03-28 23:19:06 +0700

    Create TabBar component

---
+++ client/src/components/TabBar.js
@@ -0,0 +1,37 @@
+import { View } from "react-native";
+import { useRoute } from '@react-navigation/native';
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+export default function TabBar({ focused, color, size }) {
+  var iconName = "";
+  const route = useRoute();
+  switch (route.name) {
+    case "Home":
+      iconName = focused ? "home" : "home-outline";
+      break;
+    case "Schedule":
+      iconName = focused ? "time" : "time-outline";
+      break;
+    case "Statistic":
+      iconName = focused ? "bar-chart" : "bar-chart-outline";
+      break;
+    case "Notification":
+      iconName = focused ? "notifications" : "notifications-outline";
+      break;
+    case "Profile":
+      iconName = focused ? "person" : "person-outline";
+      break;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Ionicons name={iconName} color={color} size={size} />
+    </View>
+  );
+}
