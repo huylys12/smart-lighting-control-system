4d21dbac3364acee574aaf603d5f4399bd1e952f
Huy Ly <huylg2109@gmail.com>
2023-03-29 22:24:38 +0700

    Create HomeRoom, HomeLight

--- client/src/screens/home/HomeStack.js
+++ client/src/screens/home/HomeStack.js
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeHomeScreen from "./HomeHomeScreen";
+import HomeRoomScreen from "./HomeRoomScreen";
+import HomeLightScreen from "./HomeLightScreen";
 
 const HistoryStack = createNativeStackNavigator();
 
@@ -7,6 +9,8 @@ export default function HomeStack() {
   return (
     <HistoryStack.Navigator screenOptions={{ headerShown: false }}>
       <HistoryStack.Screen name="HomeHome" component={HomeHomeScreen} />
+      <HistoryStack.Screen name="HomeRoom" component={HomeRoomScreen} />
+      <HistoryStack.Screen name="HomeLight" component={HomeLightScreen} />
     </HistoryStack.Navigator>
   );
 }
