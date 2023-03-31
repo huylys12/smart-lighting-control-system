63448f6fee76b9827e5ed66d114c40f9884707c3
Huy Ly <huylg2109@gmail.com>
2023-03-31 15:52:06 +0700

    Implement HomeRoom

--- client/src/screens/home/HomeHomeScreen.js
+++ client/src/screens/home/HomeHomeScreen.js
@@ -7,16 +7,23 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import GroupsOutlineIcon from "react-native-vector-icons/MaterialIcons";
 import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import FamilyRestroomOutlinedIcon from "react-native-vector-icons/MaterialIcons";
+import GroupsOutlineIcon from "react-native-vector-icons/MaterialIcons";
 
-export default function HomeHomeScreen({ navigation }) {
+export default function HomeHomeScreen({ navigation, route }) {
   const [isEnabled, setIsEnabled] = useState(false);
 
   const toggleSwitch = () => {
     setIsEnabled((previousState) => !previousState);
   };
+
+  const handleTapRoomItem = () => {
+    return navigation.navigate('HomeRoom', {
+      name: "Living Room"});
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.intro.container}>
@@ -41,24 +48,26 @@ export default function HomeHomeScreen({ navigation }) {
       </View>
       <ScrollView>
         <View style={styles.roomList.roomGrid}>
-          <View style={styles.roomItem.container}>
-            <View style={styles.roomItem.top}>
-              <GroupsOutlineIcon
-                name="groups"
-                type="outline"
-                size={24}
-                color="#0E0E17"
-              />
-              <Switch
-                trackColor={{ false: "#E1E1E1", true: "#384EC7" }}
-                thumbColor={"#fff"}
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-              />
+          <TouchableOpacity onPress={handleTapRoomItem}>
+            <View style={styles.roomItem.container}>
+              <View style={styles.roomItem.top}>
+                <GroupsOutlineIcon
+                  name="groups"
+                  type="outline"
+                  size={24}
+                  color="#0E0E17"
+                />
+                <Switch
+                  trackColor={{ false: "#E1E1E1", true: "#384EC7" }}
+                  thumbColor={"#fff"}
+                  onValueChange={toggleSwitch}
+                  value={isEnabled}
+                />
+              </View>
+              <Text style={styles.roomItem.title}>Living Room</Text>
+              <Text style={styles.roomItem.subtitle}>3 Lights</Text>
             </View>
-            <Text style={styles.roomItem.title}>Living Room</Text>
-            <Text style={styles.roomItem.subtitle}>3 Lights</Text>
-          </View>
+          </TouchableOpacity>
           <View style={styles.roomItem.container}>
             <View style={styles.roomItem.top}>
               <GroupsOutlineIcon
@@ -302,14 +311,14 @@ const styles = StyleSheet.create({
       },
       button: {
         container: {
-          padding: 6
+          padding: 6,
         },
         text: {
           fontWeight: 600,
           fontSize: 15,
           color: "#384EC7",
-        }
-      }
+        },
+      },
     },
     roomGrid: {
       flexDirection: "row",
