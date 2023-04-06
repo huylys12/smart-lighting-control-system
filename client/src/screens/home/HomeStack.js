2e2132e3619bd53077b32712f52de7ffb2bd2467
Huy Ly <huylg2109@gmail.com>
2023-04-06 23:59:36 +0700

    Fix bug HomeStack

--- client/src/screens/home/HomeStack.js
+++ client/src/screens/home/HomeStack.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeHomeScreen from "./HomeHomeScreen";
+import HomeRoomScreen from "./HomeRoomScreen";
 import HomeLightScreen from "./HomeLightScreen";
 import HomeAddRoomScreen from "./HomeAddRoomScreen";
 import HomeAddLightScreen from './HomeAddLightScreen';
@@ -9,6 +10,7 @@ const HistoryStack = createNativeStackNavigator();
 export default function HomeStack() {
   return (
     <HistoryStack.Navigator screenOptions={{ headerShown: false }}>
+      <HistoryStack.Screen name="HomeHome" component={HomeHomeScreen} options={{ headerShown: false }}/>
       <HistoryStack.Screen name="HomeRoom" component={HomeRoomScreen} options={{ headerShown: true }}/>
       <HistoryStack.Screen name="HomeLight" component={HomeLightScreen} options={{ headerShown: true, headerBackTitle: "Back" }}/>
       <HistoryStack.Screen name="HomeAddRoom" component={HomeAddRoomScreen} options={{ headerShown: true, headerBackTitle:"Back" }}/>
