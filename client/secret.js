0025b5d723367b47f0e88a3bc0abdc73f4d38164
Huy Ly <huylg2109@gmail.com>
2023-05-28 21:13:38 +0700

    Update ProfileHomeScreen

---
+++ client/secret.js
@@ -0,0 +1,3 @@
+const SERVER_URL = "http://172.20.10.13:3000";
+
+export {SERVER_URL};
--- client/src/screens/profile/ProfileHomeScreen.js
+++ client/src/screens/profile/ProfileHomeScreen.js
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { Avatar } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -11,22 +18,26 @@ import { useContext } from "react";
 
 export default function ProfileHomeScreen({ navigation }) {
   const { token } = useContext(AuthContext);
-  const handleLogout = async() =>{
-    const res = await api.get({url:"api/accounts/logout",token:token});
-    if(res){
-      console.log("Logout: ",res);
+
+  const handleTabEditProfile = () => {
+    return navigation.navigate("Edit Profile");
+  };
+  const handleLogout = async () => {
+    const res = await api.get({ url: "api/accounts/logout", token: token });
+    if (res) {
+      console.log("Logout: ", res);
       navigation.navigate("Welcome");
     }
-  }
+  };
   return (
     <View style={style.container}>
       <View>
         <ProfileName name={"Harry Brown"} />
         <ProfilePower poweruse={"219 kwh"} />
       </View>
-        <Text style={{ fontSize: 22, fontWeight: 600, marginTop: 16 }}>
-          Setting
-        </Text>
+      <Text style={{ fontSize: 22, fontWeight: 600, marginTop: 16 }}>
+        Setting
+      </Text>
       <ScrollView>
         <NotificationContainer
           text={"Notification"}
@@ -40,18 +51,14 @@ export default function ProfileHomeScreen({ navigation }) {
           name={"calendar-outline"}
           color={"#599BF9"}
         />
-        <NotificationContainer
-          text={"Profile"}
-          hour={"Edit information in your profile, even your password"}
-          name={"create-outline"}
-          color={"#FFAC3D"}
-        />
-        <NotificationContainer
-          text={"Profile"}
-          hour={"Edit information in your profile, even your password"}
-          name={"create-outline"}
-          color={"#FFAC3D"}
-        />
+        <TouchableOpacity onPress={handleTabEditProfile}>
+          <NotificationContainer
+            text={"Profile"}
+            hour={"Edit information in your profile, even your password"}
+            name={"create-outline"}
+            color={"#FFAC3D"}
+          />
+        </TouchableOpacity>
         <TouchableOpacity style={style.logout} onPress={handleLogout}>
           <Text
             style={{ textAlign: "center", color: "rgba(75, 97, 221, 0.5)" }}
